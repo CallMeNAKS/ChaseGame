d9a5ca9977196b8229a4991836b6d69368bfb6b0
Kirill <kirill.gurin20152016@yandex.ru>
2024-12-09 19:55:29 +0300

    Add-Health
    Health event and take damage logic

--- Assets/CodeBase/Enemy/EnemyHunter.cs
+++ Assets/CodeBase/Enemy/EnemyHunter.cs
@@ -1,14 +1,18 @@
 using System.Collections;
-using System.Collections.Generic;
+using CodeBase;
 using UnityEngine;
 using UnityEngine.AI;
 
-[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyHunter : MonoBehaviour
 {
-    [SerializeField]
-    private Transform _playerTransform;
+    [Header("Debug")]
+    [SerializeField] private int _damage = 5;
+    [SerializeField] private Transform _playerTransform;
+    private Coroutine _periodicDamageCoroutine;
     private NavMeshAgent _navMeshAgent;
+    private Collider _playerCollider;
+    
 
     private void Awake()
     {
@@ -18,5 +22,32 @@ public class EnemyHunter : MonoBehaviour
     private void Update()
     {
         _navMeshAgent.SetDestination(_playerTransform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && other.TryGetComponent<Health>(out var playerHealth))
+        {
+            _playerCollider = other;
+            _periodicDamageCoroutine = StartCoroutine(PeriodicDamage(playerHealth));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (_playerCollider == other)
+        {
+            StopCoroutine(_periodicDamageCoroutine);
+            _periodicDamageCoroutine = null;
+        }
+    }
+
+    private IEnumerator PeriodicDamage(Health playerHealth)
+    {
+        while (true)
+        {
+            playerHealth.TakeDamage(_damage);
+            yield return new WaitForSeconds(.5f);
+        }
     }
 }
---
+++ Assets/CodeBase/Health.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase
+{
+    public class Health : MonoBehaviour
+    {
+        private int _health = 100;
+        public event Action<int> HealthChanged;
+        public event Action Death;
+
+
+        private void Awake()
+        {
+            HealthChanged += OnHited;
+            Death += OnDeath;
+        }
+
+        private void OnHited(int _)
+        {
+            print("Health " + _health);
+        }
+
+        private void OnDeath()
+        {
+            print("Death");
+        }
+        
+        public void TakeDamage(int damage)
+        {
+            if (damage <= 0)
+            {
+                return;
+            }
+            
+            _health -= damage;
+
+            if (_health - damage <= 0)
+            {
+                _health = 0;
+                Death?.Invoke();
+            }
+            
+            HealthChanged?.Invoke(_health);
+        }
+    }
+}
