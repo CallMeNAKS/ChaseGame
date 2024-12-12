9293c200c01f83500cfbc62bea15703f410a8f60
CallMeNAKS <thefan51999@gmail.com>
2024-12-12 15:28:47 +0300

    upd states

---
+++ Assets/CodeBase/CatchOutState.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using CodeBase.Enemy;
+using CodeBase.GameStateMachine;
+using CodeBase.Utils;
+using UnityEngine;
+
+namespace CodeBase
+{
+    public class CatchOutState : IGameState
+    {
+        private readonly Coroutines _coroutines;
+        private readonly StateMachine _stateMachine;
+        private readonly Bonus.Bonus _bonus;
+        private readonly Player.Player _player;
+        private readonly EnemyBehavior _enemyBehavior;
+
+        private const float BONUS_DURATION = 10f;
+
+
+        public CatchOutState(StateMachine stateMachine,
+            Bonus.Bonus bonus,
+            Coroutines coroutines,
+            Player.Player player,
+            EnemyBehavior enemyBehavior)
+        {
+            _stateMachine = stateMachine;
+            _bonus = bonus;
+            _coroutines = coroutines;
+            _player = player;
+            _enemyBehavior = enemyBehavior;
+        }
+
+        public void Enter()
+        {
+            _coroutines.StartCoroutine(StateTimer());
+            _player.ToggleState();
+            _enemyBehavior.ChangeBehavior();
+        }
+
+        public void Exit()
+        {
+            _bonus.Spawn();
+            _player.ToggleState();
+            _enemyBehavior.ChangeBehavior();
+        }
+
+        private IEnumerator StateTimer()
+        {
+            yield return new WaitForSeconds(BONUS_DURATION);
+            _stateMachine.ChangeState(StateType.Chase);
+        }
+    }
+}
---
+++ Assets/CodeBase/Enemy/EnemyBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace CodeBase.Enemy
+{
+    public class EnemyBehavior
+    {
+        private readonly List<EnemyHunter> _hunters;
+
+        public EnemyBehavior(List<EnemyHunter> hunters)
+        {
+            _hunters = hunters;
+        }
+
+        public void ChangeBehavior()
+        {
+            foreach (var hunter in _hunters)
+            {
+                hunter.ToggleState();
+            }
+        }
+    }
+}
